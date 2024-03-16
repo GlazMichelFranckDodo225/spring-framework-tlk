7aeeb3bbd52dfd426e7bd64b65101d67d5f92b88
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-16 16:16:48 +0100

    Project Initialization

---
+++ src/main/java/com/dgmf/springframeworktlk/SpringFrameworkTlkApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.springframeworktlk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkTlkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkTlkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgmf/springframeworktlk/SpringFrameworkTlkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.springframeworktlk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkTlkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
